5d9ec9a4fa27bfb6748efe8afa177bf732d635e5
Mike Godfrey <mjgodfrey83@gmail.com>
2014-03-09 12:29:04 +0000

    Simple starter build process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function(grunt) {
 
    grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
+      dist: 'dist',
+      filename: 'angular-piechart',
       html2js: {
          test: {
             options: {
@@ -20,13 +22,32 @@ module.exports = function(grunt) {
             configFile: 'karma.conf.js',
             autoWatch: true
          }
+      },
+      clean: {
+         dist: ['<%= dist %>']
+      },
+      copy: {
+         dist: {
+            src: ['src/piechart.js'],
+            dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.js'
+         }
+      },
+      uglify: {
+         dist: {
+            src: ['<%= copy.dist.dest %>'],
+            dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.min.js'
+         }
       }
    });
 
    grunt.loadNpmTasks('grunt-html2js');
    grunt.loadNpmTasks('grunt-karma');
+   grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-contrib-copy');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
 
    grunt.registerTask('default', ['html2js', 'karma']);
+   grunt.registerTask('build', ['clean', 'copy', 'uglify']);
 
    return grunt;
 };
