d9e11f6d18bd338b3620fa12e99037cb4dcc0ba3
Mike Godfrey <mjgodfrey83@gmail.com>
2014-11-17 13:00:33 +0000

    Added angular as an npm package and upgraded to 1.3.2

--- karma.conf.js
+++ karma.conf.js
@@ -14,8 +14,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'misc/angular.js',
-      'misc/angular-mocks.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'src/**/*.js'
     ],
 
