735bb5f527070140c268f0cd8a6da693ac239859
Mike Godfrey <mjgodfrey83@gmail.com>
2015-01-24 19:43:52 +0000

    Added first passing piechart-slice repeater test.

--- test/piechartSpec.js
+++ test/piechartSpec.js
@@ -1,7 +1,13 @@
 describe('piechart test harness', function() {
   it('should render', function() {
     browser.get('http://127.0.0.1:8000/test/piechart.html');
+
     var paths = element.all(by.repeater('slice in slices'));
-    expect(paths.length).toEqual(2);
+
+    expect(paths.count()).toEqual(2);
+    
+    paths.each(function(path) {
+    	expect(path.getTagName()).toEqual('path');
+    });
   });
 });
